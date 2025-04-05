380284d360a843a9f59a8fd57c18d47ff464ff19
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-05 23:58:12 +0600

    File structure and global route, global prisma import export

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
